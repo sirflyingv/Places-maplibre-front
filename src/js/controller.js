b98589076c67928a935d14ad53ec05f395d42a08
sirflyingv <andthenyouadd@gmail.com>
2022-10-22 13:27:42 +0300

    global search (needs refactor to MVC))

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,27 +2,48 @@ import maplibreGl from 'maplibre-gl';
 import { AJAX, timeout } from './helpers';
 import mapView from './views/mapView';
 import mockMenuView from './views/mockMenuView';
-import { saveMarker } from './model';
+import { saveMarker, getMarkers } from './model';
+import { API_URL } from './config';
 
 const map = mapView;
 
-// const marker = AJAX(
-//   'http://127.0.0.1:3000/api/v1/places/634ac66d7d66094d4011abd8',
-//   'GET'
-// ).then((res) => {
-//   new maplibreGl.Marker().setLngLat(res.data.location.coordinates).addTo(map);
-// });
-
 const addMarker = async function (id) {
-  AJAX(
-    'http://127.0.0.1:3000/api/v1/places/634ac66d7d66094d4011abd8',
+  const { data } = await AJAX(
+    `${API_URL}/api/v1/places/634ac66d7d66094d4011abd8`,
     'GET'
-  ).then((res) => {
-    const marker = new maplibreGl.Marker()
-      .setLngLat(res.data.location.coordinates)
-      .addTo(map);
-    saveMarker(marker);
-  });
+  );
+  const marker = new maplibreGl.Marker()
+    .setLngLat(data.location.coordinates)
+    .addTo(map);
+
+  saveMarker(marker);
 };
 
-mockMenuView.addHandlerButton(addMarker);
+const removeBtnDummyHandler = function () {
+  console.log('Remove button clicked');
+};
+
+const loadQuery = async function () {
+  const queryString = mockMenuView.getQuery();
+  if (queryString.length < 3) return;
+  const resTags = await AJAX(
+    `${API_URL}/api/v1/places/search?tags=${queryString}`
+  );
+  const resName = await AJAX(
+    `${API_URL}/api/v1/places/search?inname=${queryString}`
+  );
+  const resDesc = await AJAX(
+    `${API_URL}/api/v1/places/search?indesc=${queryString}`
+  );
+  const combinedResult = [...resTags.data, ...resName.data, ...resDesc.data];
+
+  const resultUniq = [
+    ...new Map(combinedResult.map((place) => [place._id, place])).values(),
+  ];
+
+  console.log(resultUniq);
+  return resultUniq;
+};
+
+mockMenuView.addHandlerButtonLoad(loadQuery);
+mockMenuView.addHandlerButtonRemove(removeBtnDummyHandler);
--- src/js/views/mockMenuView.js
+++ src/js/views/mockMenuView.js
@@ -2,12 +2,30 @@ class MockMenuView {
   _parentElement = document.getElementById('menu');
   //   btn = this._element.querySelector('.btn-load');
 
-  addHandlerButton(handler) {
+  addHandlerButtonLoad(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--load');
       if (!btn) return;
       handler();
     });
+  }
+
+  addHandlerButtonRemove(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--remove');
+      if (!btn) return;
+      handler();
+    });
+  }
+
+  getQuery() {
+    const query = this._parentElement.querySelector('.input__query').value;
+    this._clearInput();
+    return query;
+  }
+
+  _clearInput() {
+    this._parentElement.querySelector('.input__query').value = '';
   }
 }
 
